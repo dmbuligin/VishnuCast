98a5fb362ae97aea234e855962c490f3a8b6a52b
Dmitriy Buligin <dmitriy@buligin.com>
2025-09-24 08:59:06 +0300

    bump v 1.5 feat: автозапуск HTTP/WS и WebRTC при старте приложения (микрофон по умолчанию mute)
    feat: «Выход» в шторке и в меню (закрывает окно и гасит сервисы)
    
    fix: стабильный звук при многократных подключениях/отключениях
    
    feat: индикатор уровня без клиента (MicLevelProbe) + guard-таймер самовосстановления
    
    ui: тексты статуса «Трансляция запущена/остановлена» и синхронизация UI при ротации
    
    chore: логирование WS keep-alive и сигналинга на клиенте

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
 
     val versionMajor = 1
-    val versionMinor = 4
+    val versionMinor = 5
 
     // val versionPatch = 0  // ← больше не нужен, можно вернуть при необходимости
     // versionName = "3.6"
