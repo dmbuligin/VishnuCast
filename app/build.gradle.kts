1ccff851b7e6191c4f9c836770ed297eddfaa681
Dmitriy Buligin <dmitriy@buligin.com>
2025-10-10 09:14:17 +0300

    bump v 1.7.3 fix ForegroundServiceStartNotAllowed error in some devices

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 
     val versionMajor = 1
     val versionMinor = 7
-    val versionPatch = 2
+    val versionPatch = 3
 
     // versionName = "1.7.0"
     val verName = "$versionMajor.$versionMinor.$versionPatch"
